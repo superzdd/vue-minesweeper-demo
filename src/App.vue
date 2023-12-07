8c8912959965b77ce2bb9e00c49e96dc90985a30
superzdd <476257993@qq.com>
2023-12-07 17:14:07 +0800

    Delete VueRouter for testing

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts" generic="T extends any, O extends any">
+import Play from '~/pages/Play.vue'
+</script>
+
 <template>
   <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-    <RouterView />
+    <!-- <RouterView /> -->
+    <Play />
     <TheFooter />
   </main>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import UnoCSS from 'unocss/vite'
 import VueMacros from 'unplugin-vue-macros/vite'
-import VueRouter from 'unplugin-vue-router/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 export default defineConfig({
@@ -31,7 +30,7 @@ export default defineConfig({
     }),
 
     // https://github.com/posva/unplugin-vue-router
-    VueRouter(),
+    // VueRouter(),
 
     // https://github.com/antfu/unplugin-auto-import
     AutoImport({
